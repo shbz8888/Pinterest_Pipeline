07f8542672b4b7248c80eaa6b4de1199ab5c1e34
shbz8888 <shbz8888@gmail.com>
2022-12-01 18:28:11 +0000

    code made more robust

--- streaming_consumer.py
+++ streaming_consumer.py
@@ -6,6 +6,7 @@ import os
 import pyspark.sql.functions as f
 from json import loads
 os.environ["PYSPARK_SUBMIT_ARGS"] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.3,org.postgresql:postgresql:42.5.0 streaming_consumer.py pyspark-shell'
+password = os.environ["PGADMIN4_PASSWORD"]
 kafka_topic_name = "MyFirstKafkaTopic"
 kafka_bootstrap_servers = 'localhost:9092'
 
@@ -34,15 +35,16 @@ def foreach_batch_function(df, epoch_id):
     df = df.withColumn('tag_list', f.regexp_replace("tag_list", "N,o, ,T,a,g,s, ,A,v,a,i,l,a,b,l,e", "None"))
     maxValueA = df.agg(max("follower_count")).collect()[0][0]
     #df.select(("*")).write.format("console").mode("append").save()
-    df = spark.write \
+    df.write \
         .format("jdbc") \
+        .mode("append")   \
         .option("url", "jdbc:postgresql://localhost:5432/pinterest_streaming") \
-        .option("dbtable", "experimental_data") \
+        .option("dbtable", "experimental_data_1") \
         .option("user", "postgres") \
-        .option("password", "Yoruichi786") \
+        .option("password", password) \
         .option("driver", "org.postgresql.Driver") \
         .save()
-    print(f"{maxValueA} is max follower count")
+    #print(f"{maxValueA} is max follower count")
     
 schema = StructType([
         StructField("category",StringType(),True),
