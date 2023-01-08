b95e5b9946931b546727544c548ba0fb3e504956
shbz8888 <shbz8888@gmail.com>
2023-01-08 18:13:19 +0000

    cleaned up comments

--- Spark_job.py
+++ Spark_job.py
@@ -3,7 +3,6 @@ from pyspark import SparkContext, SparkConf
 import pyspark.sql.functions as f
 import os 
 import pandas as pd
-#SparkContext.stop()
 # Adding the packages required to get data from S3  
 os.environ["PYSPARK_SUBMIT_ARGS"] = "--packages com.amazonaws:aws-java-sdk-s3:1.12.196,org.apache.hadoop:hadoop-aws:3.3.1 pyspark-shell"
 # Creating our Spark configuration
@@ -23,19 +22,16 @@ hadoopConf.set('spark.hadoop.fs.s3a.aws.credentials.provider', 'org.apache.hadoo
 
 # Create our Spark session
 spark=SparkSession(sc)
-#sc.stop()
-# Read from the S3 bucket
-df = spark.read.json("s3a://pinterestdata7b8c2d40-08c6-4eb4-8c96-2f0080c4653b/*.json") # You may want to change this to read csv depending on the files your reading from the bucket
+# Read from the AWS S3 bucket
+df = spark.read.json("s3a://pinterestdata7b8c2d40-08c6-4eb4-8c96-2f0080c4653b/*.json") 
 df1 = df.select(df["category"],df["unique_id"],df["title"],df["description"],df["tag_list"],df["follower_count"]).show()
-#df.groupby("category")
-#s_df.show()
-spark.conf.set("spark.sql.execution.arrow.pyspark.enabled", "true")
-#pysparkdf = spark.createDataFrame(df1)
-#df.toPandas()
 
+spark.conf.set("spark.sql.execution.arrow.pyspark.enabled", "true")
+
+# clean data 
 df = df.withColumn('follower_count', f.regexp_replace("follower_count", "User Info Error", "0"))
 df = df.withColumn('follower_count', f.regexp_replace("follower_count", "k", "000"))
 df = df.withColumn('follower_count', f.regexp_replace("follower_count", "M", "000000"))
 df = df.withColumn('follower_count', f.col("follower_count").cast("Int"))
 df = df.withColumn('tag_list', f.regexp_replace("tag_list", "N,o, ,T,a,g,s, ,A,v,a,i,l,a,b,l,e", "None"))
-df = df.sort("category").show()
+df = df.sort("category").show() #show dataframe 
